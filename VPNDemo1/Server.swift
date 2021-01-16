b33dd2762276a694efb10a278bbf048bbdd506bc
Ahasan Ul Haque Shuvo <ahasan.shuvo86@gmail.com>
2021-01-16 11:46:11 +0600

    new changes

---
+++ VPNDemo1/Server.swift
@@ -0,0 +1,15 @@
+//
+//  Server.swift
+//  VPNDemo1
+//
+//  Created by MacBook Pro on 1/14/21.
+//  Copyright © 2021 MacBook Pro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Server {
+    let serverName: String
+    let imageView: UIImage
+}
---
+++ VPNDemo1/ServerCell.swift
@@ -0,0 +1,26 @@
+//
+//  ServerCell.swift
+//  VPNDemo1
+//
+//  Created by MacBook Pro on 1/14/21.
+//  Copyright © 2021 MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ServerCell: UITableViewCell {
+
+    @IBOutlet weak var serverName: UILabel!
+    @IBOutlet weak var flagImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
